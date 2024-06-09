82216ad531a16c58cf905649e89bb91af1f2930a
srexrg <sreeragp276@gmail.com>
2024-06-09 19:14:25 +0530

    chore: Added Analytics for page tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react";
 import { Toaster } from "@/components/ui/toaster";
 
 const IBMPlex = IBM_Plex_Sans({
@@ -30,7 +31,8 @@ export default function RootLayout({
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
           {children}
-      <Toaster />
+          <Analytics />
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
